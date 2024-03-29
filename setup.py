b4a30e604b1e42fdee7dfede829dc87ea579d092
Rahul Maurya <thegalaxy.edu@gmail.com>
2024-03-29 22:22:32 +0530

    final updates app

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
 
-HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str) -> List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function returns a list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.strip() for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
 setup(
-name = 'stdscr_project',
-version='0.0.1',
-author='Rahul_Maurya',
-author_email='thegalaxy.edu@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
+    name='stdscr_project',
+    version='0.0.1',
+    author='Rahul_Maurya',
+    author_email='thegalaxy.edu@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
